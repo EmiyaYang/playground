082664ab7e00b057acb3acd708389fdd27540c88
yangjiaqi2 <yangjiaqi2@yy.com>
2020-01-26 17:05:48 +0800

    fix: test instance

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,15 @@
-// 7 个月
-const MONTH = ["E", "F", "A", "X", "Y", "Z"];
+// 纪元开始时间戳
+export const START_TIMESTAMP = 1579536000000;
+
+export enum MonthSymbol {
+  E = 0,
+  F,
+  G,
+  A,
+  X,
+  Y,
+  Z
+}
 
 export enum TimeUnit {
   SECOND = "second",
@@ -10,44 +20,154 @@ export enum TimeUnit {
   YEAR = "year"
 }
 
-function getTimeMap(timestamp: number) {
-  const arr = [
-    { unit: TimeUnit.SECOND, digit: 1000 },
-    { unit: TimeUnit.MINUTE, digit: 60 },
-    { unit: TimeUnit.HOUR, digit: 28 },
-    { unit: TimeUnit.DAY, digit: 12 },
-    { unit: TimeUnit.MONTH, digit: 17 },
-    { unit: TimeUnit.YEAR, digit: 7 }
-  ];
-
-  return arr.map(({ unit, digit }, index) => {
-    let value = Math.floor(timestamp / digit);
-    const nextDigit = (arr[index + 1] && arr[index + 1].digit) || 0;
-
-    if (nextDigit) {
-      value %= nextDigit;
-    }
-
-    return { unit, value };
-  });
-}
+const DigitArr = [
+  { unit: TimeUnit.SECOND, preDigit: 1000 },
+  { unit: TimeUnit.MINUTE, preDigit: 60 },
+  { unit: TimeUnit.HOUR, preDigit: 28 },
+  { unit: TimeUnit.DAY, preDigit: 12 },
+  { unit: TimeUnit.MONTH, preDigit: 17 },
+  { unit: TimeUnit.YEAR, preDigit: 7 }
+];
 
 export class YMoment {
   timestamp: number;
-  timeArr: Array<{ unit: TimeUnit; value: number }>;
+  timeMap: {
+    [key in TimeUnit]: { value: number; preDigit: number; nextDigit: number };
+  };
 
   constructor(timestamp: number) {
     this.timestamp = timestamp;
-    this.timeArr = getTimeMap(this.timestamp);
+    this.timeMap = serialize(this.timestamp);
   }
 
   get(type: TimeUnit) {
-    const time = this.timeArr.find(item => item.unit === type);
+    return this.timeMap[type] && this.timeMap[type].value;
+  }
 
-    return (time && time.value) || undefined;
+  getDayOfWeek() {
+    return new Date(this.timestamp).getDay();
+  }
+
+  // 输出示例: A070017 07:24:10
+  format() {
+    const month = MonthSymbol[this.timeMap.month.value];
+    const day = zeroPadding(this.timeMap.day.value);
+    const year = zeroPadding(this.timeMap.year.value, 4);
+    const hour = zeroPadding(this.timeMap.hour.value);
+    const minute = zeroPadding(this.timeMap.minute.value);
+    const second = zeroPadding(this.timeMap.second.value);
+
+    return `${month}${day}${year} ${hour}:${minute}:${second}`;
   }
 
   valueOf() {
     return this.timestamp;
   }
+}
+
+/**
+ * 将不同单位的时间字符串转为数字
+ */
+export function getValue(value: number, unit: TimeUnit) {
+  if (value === 0) return value;
+
+  let index = DigitArr.findIndex(value => value.unit === unit);
+
+  if (index < 0 || value < 0) throw new TypeError();
+
+  let res = value;
+
+  do {
+    const digit = DigitArr[index].preDigit;
+
+    res *= digit;
+  } while (index-- > 0);
+
+  return res;
+}
+
+/**
+ * 将时间串转化为时间戳
+ * @param str
+ */
+export function parse(str: string): number {
+  const arr = JSON.parse(JSON.stringify(DigitArr));
+  arr.reverse();
+
+  const inputOrder = [
+    TimeUnit.MONTH,
+    TimeUnit.DAY,
+    TimeUnit.YEAR,
+    TimeUnit.HOUR,
+    TimeUnit.MINUTE,
+    TimeUnit.SECOND
+  ];
+  const reg = /(\w)(\d{2})(\d{4})\s(\d{2}):(\d{2}):(\d{2})/;
+
+  return (
+    inputOrder.reduce((acc, current, index) => {
+      let value = 0;
+      const char = str.replace(reg, "$" + (index + 1));
+
+      if (current === TimeUnit.MONTH) {
+        value = Number(MonthSymbol[<any>char]);
+      } else if (current === TimeUnit.DAY) {
+        value = Number(char) - 1;
+      } else {
+        value = Number(char);
+      }
+
+      return acc + getValue(value, current);
+    }, 0) + START_TIMESTAMP
+  );
+}
+
+/**
+ * 将时间戳格式化
+ * @param timestamp
+ */
+export function format(timestamp: number) {
+  const timeMap = serialize(timestamp);
+
+  const month = MonthSymbol[timeMap.month.value];
+  // 每月从第一天开始
+  const day = zeroPadding(timeMap.day.value + 1);
+  const year = zeroPadding(timeMap.year.value, 4);
+  const hour = zeroPadding(timeMap.hour.value);
+  const minute = zeroPadding(timeMap.minute.value);
+  const second = zeroPadding(timeMap.second.value);
+
+  return `${month}${day}${year} ${hour}:${minute}:${second}`;
+}
+
+function serialize(timestamp: number) {
+  if (timestamp < START_TIMESTAMP) throw new TypeError("黄元前的就不算了.");
+
+  let value = timestamp - START_TIMESTAMP;
+
+  return DigitArr.reduce((acc: any, { unit, preDigit }, index) => {
+    value = Math.floor(value / preDigit);
+    const nextDigit =
+      (DigitArr[index + 1] && DigitArr[index + 1].preDigit) || 0;
+
+    let resValue = nextDigit ? value % nextDigit : value;
+
+    acc[unit] = {
+      value: resValue,
+      preDigit,
+      nextDigit
+    };
+
+    return acc;
+  }, {});
+}
+
+export function zeroPadding(value: number | string, digit: number = 2): string {
+  if (digit <= 0) throw new TypeError();
+
+  let res = String(value);
+
+  while (res.length < digit) res = "0" + res;
+
+  return res;
 }
--- tests/unit/utils.spec.ts
+++ tests/unit/utils.spec.ts
@@ -1,14 +1,113 @@
-import { YMoment, TimeUnit } from "@/utils";
+import {
+  YMoment,
+  TimeUnit,
+  zeroPadding,
+  START_TIMESTAMP,
+  getValue,
+  MonthSymbol,
+  parse,
+  format
+} from "@/utils";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const timestamp = Date.now();
+describe("YMoment", () => {
+  let timestamp = Date.now();
 
-    const moment = new YMoment(timestamp);
+  let moment = new YMoment(timestamp);
 
+  beforeEach(() => {
+    timestamp = Date.now();
+    moment = new YMoment(timestamp);
+  });
+
+  it("common", () => {
     expect(moment.valueOf()).toEqual(timestamp);
-    expect(moment.get(TimeUnit.SECOND)).toEqual(
-      new Date(timestamp).getSeconds()
+
+    expect(moment.getDayOfWeek()).toEqual(new Date(timestamp).getDay());
+  });
+
+  it("get", () => {
+    const ts = timestamp - START_TIMESTAMP;
+
+    let value = Math.floor(ts / 1000);
+    value %= 60;
+
+    expect(moment.get(TimeUnit.SECOND)).toEqual(value);
+
+    value = Math.floor(ts / 1000 / 60);
+    value %= 28;
+    expect(moment.get(TimeUnit.MINUTE)).toEqual(value);
+
+    value = Math.floor(ts / 1000 / 60 / 28);
+    value %= 12;
+    expect(moment.get(TimeUnit.HOUR)).toEqual(value);
+
+    value = Math.floor(ts / 1000 / 60 / 28 / 12);
+    value %= 17;
+    expect(moment.get(TimeUnit.DAY)).toEqual(value);
+
+    value = Math.floor(ts / 1000 / 60 / 28 / 12 / 17);
+    value %= 7;
+    expect(moment.get(TimeUnit.MONTH)).toEqual(value);
+
+    value = Math.floor(ts / 1000 / 60 / 28 / 12 / 17 / 7);
+    expect(moment.get(TimeUnit.YEAR)).toEqual(value);
+  });
+
+  it("parse", () => {
+    const sampleA = "E010000 00:00:00";
+    const sampleB = "E010001 00:00:00";
+    const sampleC = "A070017 07:24:10";
+
+    expect(parse(sampleA)).toEqual(START_TIMESTAMP);
+    expect(parse(sampleB)).toEqual(
+      START_TIMESTAMP + 7 * 17 * 12 * 28 * 60 * 1000
+    );
+    expect(parse(sampleC)).toEqual(
+      START_TIMESTAMP +
+        getValue(MonthSymbol.A, TimeUnit.MONTH) +
+        getValue(7 - 1, TimeUnit.DAY) +
+        getValue(17, TimeUnit.YEAR) +
+        getValue(7, TimeUnit.HOUR) +
+        getValue(24, TimeUnit.MINUTE) +
+        getValue(10, TimeUnit.SECOND)
+    );
+  });
+
+  it("format", () => {
+    const sampleA = "E010000 00:00:00";
+    const sampleB = "E010001 00:00:00";
+    const sampleC = "A070017 07:24:10";
+
+    const timestampA = parse(sampleA);
+    const timestampB = parse(sampleB);
+    const timestampC = parse(sampleC);
+
+    expect(format(timestampA)).toEqual(sampleA);
+    expect(format(timestampB)).toEqual(sampleB);
+    expect(format(timestampC)).toEqual(sampleC);
+  });
+
+  // it("two directions", () => {
+  //   expect(moment.parse(moment.format())).toEqual(timestamp);
+  // });
+});
+
+describe("otherUtils", () => {
+  it("zeroPadding", () => {
+    expect(zeroPadding(1)).toEqual("01");
+    expect(zeroPadding(1, 3)).toEqual("001");
+  });
+
+  it("getValue", () => {
+    expect(getValue(0, TimeUnit.SECOND)).toEqual(0);
+    expect(getValue(1, TimeUnit.SECOND)).toEqual(1000);
+    expect(getValue(1, TimeUnit.MINUTE)).toEqual(1000 * 60);
+    expect(getValue(1, TimeUnit.HOUR)).toEqual(1000 * 60 * 28);
+    expect(getValue(1, TimeUnit.DAY)).toEqual(1000 * 60 * 28 * 12);
+    expect(getValue(1, TimeUnit.MONTH)).toEqual(1000 * 60 * 28 * 12 * 17);
+    expect(getValue(1, TimeUnit.YEAR)).toEqual(1000 * 60 * 28 * 12 * 17 * 7);
+    expect(getValue(2, TimeUnit.YEAR)).toEqual(
+      1000 * 60 * 28 * 12 * 17 * 7 * 2
     );
   });
 });
