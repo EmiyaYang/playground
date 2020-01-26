f9eca43858a4ce5ea3a9b34256e0dbf8c7aa2062
EmiyaYang <1038810929@qq.com>
2020-01-26 20:59:36 +0800

    fix: depley config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/playground/dist" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/playground/dist/" : "/"
 };
